80f64d53dcfbca38010a6ccd4d51bfbc82419508
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-11-27 19:36:25 -0700

    Feat: Added Function to fetch news Articles based on the given topic

--- functions/apiHelpers.js
+++ functions/apiHelpers.js
@@ -11,7 +11,39 @@ const getYesterdayISO = () => {
 
 // Fetch Articles using the NewAPI.org Everything endpoint
 const getArticles = async (topic) => {
+  const apiKey = process.env.NEWS_API_KEY;
+  if(!apiKey) {
+    throw new Error('Missing NEWS_API_KEY');
+  }
 
+  // This will enforce a 5-word limit for the topic. Frontend will also enforce this
+  const sanitizedTopic = topic.split(' ').slice(0, 5).join(' ');
+
+  const fromDate = getYesterdayISO();
+
+  /***
+    Endpoint: /v2/everything
+    Parameters:
+    q: The user's topic
+    from: Only articles from the last 24 hours
+    sortBy: 'popularity' finds the most popular articles first
+    language: 'en' restricts to English
+  ***/
+  const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(sanitizedTopic)}&from=${fromDate}&sortBy=popularity&language=en&pageSize=5&apiKey=${apiKey}`;
+
+  logger.info('Fetching articles using the URL: ', url);
+
+  try {
+    const response = await axios.get(url);
+    if(response.data.status !== 'ok') {
+      throw new Error(`News API response Error:  ${response.data.message}`);
+    }
+
+    return response.data.articles;
+  }catch (error) {
+    logger.error("NewsAPI Failed", error);
+    return []; // returning empty so the app wont crash
+  }
 }
 
 // handle NesAPI and FireCrawl logic
