21017307975d1900dae1408d4def846957c1517c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-11-29 14:25:54 -0700

    Feat: Implement getLatesDigest function

--- src/services/DigestService.js
+++ src/services/DigestService.js
@@ -22,3 +22,30 @@ export const triggerManualDigest = async () => {
     throw e;
   }
 }
+
+/**
+ * Firebase Index needs to be created for this function to work
+ *
+ * */
+export const getLatestDigest = async (userId) => {
+  try{
+    const digestRef = collection(db, "digest");
+
+    const queryToGetDigest = query(
+      digestRef,
+      where('userId', '==', userId),
+      orderBy('createdAt', 'desc'),
+      limit(1)
+    );
+
+    const snapshot = await getDocs(queryToGetDigest);
+
+    if(snapshot.empty) return null;
+
+    return snapshot.docs[0].data;
+
+  } catch (e) {
+    console.error("Failed to getLatestDigest: ", e);
+    throw e;
+  }
+}
